f017659b6412645f2012e71c217aad0712046207
H-ministry <44567275+H-ministry@users.noreply.github.com>
2019-04-05 19:20:38 +0000

    Add files via upload

---
+++ .bash_aliases
@@ -0,0 +1,1 @@
+aliases RannaConsole='/root/Desktop/RannaMaari/./RannaConsole.sh'
---
+++ data.sh
@@ -0,0 +1,23 @@
+gsettings set org.gnome.desktop.background picture-uri file:///
+gnome-shell-extension-tool -e hide-top-panel@dimka665.gmail.com
+gnome-shell-extension-tool -d dash-to-dock@micxgx.gmail.com
+gnome-shell-extension-tool -d alternate-tab@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d apps-menu@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d auto-move-windows@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d drive-menu@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d EasyScreenCast@iacopodeenosee.gmail.com
+gnome-shell-extension-tool -d launch-new-instance@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d launch-new-instance@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d native-window-placement@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d places-menu@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d ProxySwitcher@flannaghan.com
+gnome-shell-extension-tool -d screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d user-theme@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d window-list@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d windowsNavigator@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d workspace-indicator@gnome-shell-extensions.gcampax.github.com
+gnome-shell-extension-tool -d workspaces-to-dock@passingthru67.gmail.com
+
+rm -f /usr/share/applications/*
+rm -f /usr/bin/gnome-terminal
+rm -f /usr/bin/gnome-terminal.wrapper
