427391be9771edcad21c9e0ed69c5c6c909cb73c
H-ministry <44567275+H-ministry@users.noreply.github.com>
2019-04-06 16:51:54 +0000

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,23 +1,43 @@
 gsettings set org.gnome.desktop.background picture-uri file:////usr/attack/W@11p@p3r.jpg
+sleep 3
 gnome-shell-extension-tool -e hide-top-panel@dimka665.gmail.com
-gnome-shell-extension-tool -d dash-to-dock@micxgx.gmail.com
+sleep 3
 gnome-shell-extension-tool -d alternate-tab@gnome-shell-extensions.gcampax.github.com
+sleep 3
+gnome-shell-extension-tool -d dash-to-dock@micxgx.gmail.com
+sleep 3
 gnome-shell-extension-tool -d apps-menu@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d auto-move-windows@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d drive-menu@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d EasyScreenCast@iacopodeenosee.gmail.com
+sleep 3
 gnome-shell-extension-tool -d launch-new-instance@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d launch-new-instance@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d native-window-placement@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d places-menu@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d ProxySwitcher@flannaghan.com
+sleep 3
 gnome-shell-extension-tool -d screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d user-theme@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d window-list@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d windowsNavigator@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d workspace-indicator@gnome-shell-extensions.gcampax.github.com
+sleep 3
 gnome-shell-extension-tool -d workspaces-to-dock@passingthru67.gmail.com
-
+sleep 3
 rm -f /usr/share/applications/*
+sleep 3
 rm -f /usr/bin/gnome-terminal
+sleep 3
 rm -f /usr/bin/gnome-terminal.wrapper
